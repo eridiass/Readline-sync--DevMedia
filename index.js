fbfc92c8c2ce0929c3a36b79680c1d8daecbb3ac
Erivaldo Dias <edias1991@gmail.com>
2022-11-15 19:16:01 -0300

    insercao de comentarios

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Importação do readline + cabeçalho
 import dadosEntrada from 'readline-sync'
 console.log("Área do triângulo: \n");
 
+//Declaração de principais variaveis
 const base = dadosEntrada.question("Informe o valor da base: ");
 const altura = dadosEntrada.question("Informe o valor da altura: ");
 
+//Apresentação de dados digitados em tela
 console.log("\nVocê informou " + base + " para base");
 console.log("Você informou " + altura + " para altura");
 
+//Declaração de variável + apresentação de resultados
 let area = (base * altura) / 2;
 console.log("A área do triângulo é: " + area);
